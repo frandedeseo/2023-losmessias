a4ce42aeca02d213fafcbe405ae3982af29ce889
AugusR7 <augusramseyer197@gmail.com>
2023-09-14 21:37:35 -0300

    Project creation

---
+++ BE/src/test/java/com/losmessias/pid2023/Pid2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losmessias.pid2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pid2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FE/src/pages/index.js
@@ -0,0 +1,5 @@
+import Validator from "./validator";
+
+export default function Home() {
+    return <Validator />;
+}
